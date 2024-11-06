a809553614c1b3d176358af2680a2bc8282ce8aa
Ranjeth <ranjethravichandran@hotmail.com>
2024-11-06 03:01:18 +0000

    first commit (WIP)

---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+// pages/_app.tsx
+import '../styles/global.css';
+import type { AppProps } from 'next/app';
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+    return (
+        <div className="main-container">
+            <main>
+                <Component {...pageProps} />
+            </main>
+
+            <footer>
+                <p>© 2024 Ranjeth Ravichandran</p>
+            </footer>
+        </div>
+    );
+}
