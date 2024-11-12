e2b7fd22d4732f56282881d9d9ef028748b52007
Ranjeth <ranjethravichandran@hotmail.com>
2024-11-12 21:51:53 +0000

    Configuring Next.js COnfig for Production

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+/* Conditional statement so build can work in production and locally. */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: isProd ? '/educational-platform' : '',
+  assetPrefix: '/educational-platform',
+  publicRuntimeConfig: {
+    basePath: '/educational-platform',
+  },
+  output: 'export',
   images: {
     remotePatterns: [
       {
